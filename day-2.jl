d5589907c315a098f98c980904b9556f83960a4b
Logan Kilpatrick <23kilpatrick23@gmail.com>
2022-12-05 22:27:13 -0800

    Create day-2.jl

---
+++ day-2.jl
@@ -0,0 +1,14 @@
+# Day 2: Basic syntax and data types
+
+# Learn the basics of the Julia syntax, including data types, variables, and operators
+# Practice using the REPL (read-eval-print loop) to run basic calculations and manipulate data
+# Example code:
+
+# Define a variable and assign it a value
+x = 5
+
+# Use an operator to perform a calculation
+y = x + 3
+
+# Print the result of the calculation to the screen
+println(y)

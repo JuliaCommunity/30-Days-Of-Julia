e72254430539919d7eed3dc953ed25fb3228327a
Logan Kilpatrick <23kilpatrick23@gmail.com>
2022-12-05 22:27:54 -0800

    Create day-3.jl

---
+++ day-3.jl
@@ -0,0 +1,14 @@
+# Day 3: Control flow and functions
+
+# Learn about control flow statements, such as if, else, and for loops, and how to use them to control the flow of your code
+# Learn how to define and use functions in Julia, including built-in functions and user-defined functions
+# Example code:
+
+# Define a function that takes two arguments and returns their sum
+function add(x, y)
+    return x + y
+end
+
+# Call the function with different arguments and print the results
+println(add(1, 2))
+println(add(3, 4))

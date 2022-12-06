44067228d60ee6d09c58731e91fa2f274d11360f
Logan Kilpatrick <23kilpatrick23@gmail.com>
2022-12-05 22:29:08 -0800

    Create day-5.jl

---
+++ day-5.jl
@@ -0,0 +1,13 @@
+# Day 5: Strings and string operations
+
+# Learn about strings in Julia, including how to create and manipulate them
+# Practice working with strings using built-in functions and operations
+# Example code:
+
+# Define a string and print it to the screen
+s = "Hello, world!"
+println(s)
+
+# Use string functions to manipulate the string
+t = replace(s, "world", "Julia")
+println(t)

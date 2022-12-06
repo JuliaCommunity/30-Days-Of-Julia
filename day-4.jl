5a556dc4bbbd6661beb07f62da8c08e5bb1510a6
Logan Kilpatrick <23kilpatrick23@gmail.com>
2022-12-05 22:28:23 -0800

    Create day-4.jl

---
+++ day-4.jl
@@ -0,0 +1,16 @@
+# Day 4: Arrays and matrices
+
+# Learn about arrays and matrices in Julia, including how to create, access, and manipulate them
+# Practice working with arrays and matrices using built-in functions and operations
+# Example code:
+
+# Define a matrix with 3 rows and 2 columns
+A = [1 2; 3 4; 5 6]
+
+# Access a specific element in the matrix
+println(A[1, 2])
+
+# Perform a matrix multiplication and print the result
+B = [7 8; 9 10; 11 12]
+C = A * B
+println(C)

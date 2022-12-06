1a91af91631d86c83363fc54554f4534f650feaa
Logan Kilpatrick <23kilpatrick23@gmail.com>
2022-12-05 22:26:35 -0800

    Create day-1.jl

---
+++ day-1.jl
@@ -0,0 +1,8 @@
+# Day 1: Introduction to programming and installation of Julia
+
+# Learn about the basics of programming, including what it is and why it is useful
+# Install Julia on your computer and set up a development environment
+# Example code:
+
+# Print "Hello, world!" to the screen
+println("Hello, world!")
